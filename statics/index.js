086b41c3d32ee07318188ddb10d9c3b9fbc65c01
DESKTOP-L5B3MGM\test <kanrinmaru0612@icloud.com>
2024-06-27 05:09:02 +0900

    test

--- statics/index.js
+++ statics/index.js
@@ -30,3 +30,52 @@ jQuery(function($){
         }
     });
 });
+
+
+$(document).ready(function() {
+    // .osu_circle要素を取得
+    var osuCircles = $('.osu_circle');
+
+    // スクロールイベントを監視
+    $(window).on('scroll', function() {
+        var scrollTop = $(this).scrollTop();
+        var windowHeight = $(this).height();
+
+        // 各osu_circle要素について
+        osuCircles.each(function() {
+            var elementTop = $(this).offset().top;
+
+            // 要素が表示範囲に入った場合、アニメーションを適用
+            if (scrollTop + windowHeight > elementTop) {
+                $(this).addClass('fadeInUp');
+            }
+        });
+    });
+
+    // 初期ロード時にもアニメーションを適用
+    $(window).trigger('scroll');
+});
+
+$(document).ready(function() {
+    // .osu_circle要素を取得
+    var virusItems = $('.virus_item');
+
+    // スクロールイベントを監視
+    $(window).on('scroll', function() {
+        var scrollTop = $(this).scrollTop();
+        var windowHeight = $(this).height();
+
+        // 各osu_circle要素について
+        virusItems.each(function() {
+            var elementTop = $(this).offset().top;
+
+            // 要素が表示範囲に入った場合、アニメーションを適用
+            if (scrollTop + windowHeight > elementTop) {
+                $(this).addClass('fadeInUp');
+            }
+        });
+    });
+
+    // 初期ロード時にもアニメーションを適用
+    $(window).trigger('scroll');
+});
